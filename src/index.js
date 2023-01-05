d9d3d3316612d375a85b966a77d6e671d33119ae
vitali007tut <vitali007@tut.by>
2023-01-05 21:25:28 +0300

    create: add reverse solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const absNumberString = Math.abs(n).toString();
+    let newNumberString = '';
+
+    for (let i = absNumberString.length - 1; i >= 0; i--) {
+        newNumberString += absNumberString[i];
+    }
+    return newNumberString;
 }
